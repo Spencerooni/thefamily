5ca7d1c426c77dc27e2d8a38a346770a68fc4b39
seano <s.ohara@kainos.com>
2016-10-13 14:36:17 +0100

    Added BU, Employee_Type and Sales classes

---
+++ TheFamily/src/Employee_Type.java
@@ -0,0 +1,27 @@
+/**
+ * Created by seano on 13/10/2016.
+ * TheFamily - table class for Employee_Type.
+ */
+
+public class Employee_Type {
+
+    private int Type_id;
+    private String Type_name;
+
+    public int getType_id() {
+        return Type_id;
+    }
+
+    public void setType_id(int type_id) {
+        Type_id = type_id;
+    }
+
+    public String getType_name() {
+        return Type_name;
+    }
+
+    public void setType_name(String type_name) {
+        Type_name = type_name;
+    }
+
+}
---
+++ TheFamily/src/Sales.java
@@ -0,0 +1,45 @@
+/**
+ * Created by seano on 13/10/2016.
+ * TheFamily - table class for Sales.
+ */
+
+public class Sales {
+
+    private int Sales_id;
+    private double Commission_rate;
+    private double Sales_total;
+    private int Type_id;
+
+    public int getSales_id() {
+        return Sales_id;
+    }
+
+    public void setSales_id(int sales_id) {
+        Sales_id = sales_id;
+    }
+
+    public double getCommission_rate() {
+        return Commission_rate;
+    }
+
+    public void setCommission_rate(double commission_rate) {
+        Commission_rate = commission_rate;
+    }
+
+    public double getSales_total() {
+        return Sales_total;
+    }
+
+    public void setSales_total(double sales_total) {
+        Sales_total = sales_total;
+    }
+
+    public int getType_id() {
+        return Type_id;
+    }
+
+    public void setType_id(int type_id) {
+        Type_id = type_id;
+    }
+
+}
