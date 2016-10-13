208d0218a174a69fb4723d384d27c34878555b75
Eamon Compston <eamonc@inferno.local>
2016-10-13 14:30:44 +0100

    Added employee class

---
+++ TheFamily/src/Employee.java
@@ -0,0 +1,47 @@
+/**
+ * Created by eamonc on 13/10/2016.
+ */
+public class Employee {
+        private int Employee_id;
+        private String Name;
+        private String Address;
+        private String Ni_num;
+        private String IBAN_num;
+        private double Salary;
+
+        public int getId() {
+            return Employee_id;
+        }
+        public void setId(int id) {
+            this.Employee_id = id;
+        }
+
+        public String getName() {
+            return Name;
+        }
+        public void setName(String name) {
+            this.Name = name;
+        }
+
+        public String getAddress() {
+            return Address;
+        }
+        public void setAddress(String address) {
+            this.Address = address;
+        }
+
+        public String getNi_num() {
+            return Ni_num;
+        }
+        public void setNi_num(String ni_num) {
+            this.Ni_num = ni_num;
+        }
+
+        public double getSalary() {
+            return Salary;
+        }
+        public void setSalary(int salary) {
+            this.Salary = salary;
+        } 
+
+}
